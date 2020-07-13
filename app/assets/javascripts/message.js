05a12ffedce721fcabf4def8a765d9040eb56160
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-07-13 22:50:20 +0900

    add ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,19 @@
+$(function(){
+  $(".form").on("submit", function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    .done(function(data){
+      let html = buildHTML(data);
+      $(".")
+    })
+    })
+  })
+});
