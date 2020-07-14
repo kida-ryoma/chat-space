6777b9eab4a762c752137e78534842129d912211
kida-ryoma <59079168+kida-ryoma@users.noreply.github.com>
2020-07-14 13:00:29 +0900

    finalize ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,43 @@
 $(function(){
+  
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+     `<div class="message-block">
+        <div class="info">
+          <div class="info__name">
+          ${message.user_name}
+          </div>
+        <div class="info__data">
+          ${message.created_at}
+        </div>
+        </div>
+          <div class="message-block__contents">
+            <p class="message-block__contents__content">${message.text}</p>
+            <img class="message-block__contents__image" src="${message.image}"></img>
+          </div>
+      </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message-block">
+        <div class="info">
+          <div class="info__name">
+            ${message.user_name}
+          </div>
+          <div class="info__data">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-block__contents">
+          <p class="message-block__contents__content"></p>
+          ${message.text}
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
   $(".form").on("submit", function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,10 +49,16 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
     .done(function(data){
       let html = buildHTML(data);
-      $(".")
+      $(".message-list").append(html);
+      $('form')[0].reset();
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      $('.form__send').prop('disabled', false);
     })
+    .fail(function(){
+      alert("error");
     })
   })
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,10 +1,4 @@
-# json.text @message.text
-# json.image @message.image_url
-# json.user_name @message.user.name
-# json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
-
 json.user_name @message.user.name
-
 json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
 json.text @message.text
-json.image @message.image_url
+json.image @message.image.url
